de28921bb2035f8ac6a2627ec616ae76eedfc3d4
Jacob Ploog <jploog066@west-mec.org>
2025-01-08 14:26:46 -0700

    next

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,105 @@
 // src/App.jsx
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 // import Home from "./pages/Home";
+import styles from "./styles/Home.module.css";
 
 import "./App.css";
 export default function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <Navbar />
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id nisi
+          quis mauris lobortis dapibus. Duis iaculis nulla vitae tellus mollis
+          sagittis. Curabitur id leo non dui cursus ultrices. Vestibulum
+          volutpat id ante convallis sollicitudin. Nullam dapibus vestibulum
+          bibendum. Nam feugiat vehicula egestas. Fusce metus diam, faucibus
+          feugiat convallis ut, molestie ut purus. Ut tincidunt massa elementum,
+          tristique tortor varius, mollis metus. Sed et dolor nulla. Aenean
+          luctus eros tortor, eget porttitor metus ultrices malesuada.
+          Pellentesque pharetra, sapien vel efficitur suscipit, est nibh congue
+          felis, a maximus leo tortor et velit. Integer dapibus, tellus ut
+          sagittis euismod, tortor dolor euismod quam, quis pellentesque turpis
+          est non odio. Maecenas vel molestie lectus. Nullam ex quam, bibendum
+          nec scelerisque quis, interdum in velit. Etiam imperdiet eget purus
+          quis vehicula. Aliquam consequat diam eleifend enim dapibus, sed
+          tempor quam ultricies. Etiam lobortis congue convallis. Nulla ut
+          turpis felis. Vestibulum ante ipsum primis in faucibus orci luctus et
+          ultrices posuere cubilia curae; Fusce dignissim, nisi non aliquam
+          dictum, erat sem feugiat lorem, ac ullamcorper lacus felis at ex.
+          Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
+          egestas convallis aliquet. Ut fringilla dignissim felis, nec tincidunt
+          dui rutrum eu. Nulla tincidunt tellus nisi. Duis a augue consequat,
+          semper leo id, faucibus eros. In id dignissim arcu, gravida tincidunt
+          nulla. Maecenas pulvinar felis nec semper aliquet. Nulla condimentum
+          mi vestibulum purus rutrum, sit amet lobortis ligula pellentesque.
+          Proin sit amet eros nibh. Aliquam non malesuada dolor, at imperdiet
+          lacus. Cras placerat lorem condimentum cursus aliquam. Nulla egestas
+          blandit magna, in pretium leo pulvinar ac. In viverra sapien ipsum, ut
+          placerat nisl finibus elementum. Donec quis urna sem. Vestibulum id
+          arcu non metus accumsan interdum. Maecenas tincidunt lacus a urna
+          faucibus bibendum. Ut sit amet magna sed massa scelerisque dapibus.
+          Donec faucibus est quis ex aliquet, eu cursus quam ornare. Praesent eu
+          nisi congue, imperdiet est nec, malesuada ipsum. Integer ac purus quis
+          enim dignissim molestie. Vestibulum convallis sapien quis ornare
+          efficitur. Fusce accumsan, nisl nec sagittis fermentum, ante lacus
+          mattis massa, a aliquet dui ipsum in orci. Aliquam non pellentesque
+          nibh. Sed vehicula lobortis libero, vel venenatis tellus sagittis
+          quis. Pellentesque pharetra arcu ac ante imperdiet, quis sollicitudin
+          augue tempor. Fusce rhoncus pulvinar risus, sit amet imperdiet risus
+          venenatis eget. Sed placerat turpis nec tempor pretium. Nunc orci
+          arcu, faucibus ultrices vulputate et, eleifend at lectus. Sed tortor
+          velit, lacinia ac ex vitae, hendrerit posuere velit. Nunc sed turpis
+          neque. Donec posuere sapien vel enim laoreet molestie. In ac erat sed
+          eros bibendum iaculis. Ut tincidunt cursus mauris eget feugiat. Mauris
+          vulputate nunc turpis, tempor ultricies quam suscipit id. Etiam at
+          tellus ac magna gravida ultricies. Proin ac varius urna. Morbi vel
+          turpis elit. Sed in nisl risus. Nunc sit amet odio consectetur,
+          convallis sapien sit amet, luctus dui. Quisque finibus hendrerit
+          vulputate. Sed in nisi quis turpis auctor commodo ac eget nibh. Nulla
+          pellentesque, felis vel iaculis porta, massa nunc malesuada sem, ut
+          bibendum lectus nunc vitae lorem. Curabitur ac ipsum tristique,
+          vestibulum augue a, convallis dolor. Aenean eu vestibulum mi. Sed
+          suscipit sem nec nulla iaculis, non suscipit ipsum laoreet. Nulla
+          ullamcorper blandit tincidunt. Suspendisse vel augue non velit
+          imperdiet consectetur. Maecenas consequat augue quam, vitae finibus
+          magna mollis nec. Mauris quis erat quam. Sed aliquet erat nec mi
+          porta, id hendrerit tellus congue. Donec et ipsum congue, placerat
+          dolor ac, dignissim leo. Vivamus sed iaculis quam, et aliquet massa.
+          Sed semper consectetur interdum. Proin ante sem, consequat eu varius
+          et, rhoncus eu lacus. In quam est, dictum at nunc bibendum, mattis
+          elementum dui. Curabitur semper porttitor mauris at convallis. Vivamus
+          sagittis metus et metus mollis pulvinar in ac erat. Suspendisse
+          interdum lacus finibus, aliquet purus et, pretium nunc. Aliquam et mi
+          in velit ornare congue. Integer interdum aliquam nibh vitae volutpat.
+          Etiam nec eros eget nibh maximus rutrum a a orci. Vivamus nisi quam,
+          pharetra non congue a, varius ac enim. Nulla nec ligula maximus,
+          vestibulum dui a, mollis sapien. Duis cursus, elit in tempor porta,
+          lorem nunc luctus ligula, quis euismod purus nulla a nisi. Fusce
+          rhoncus elit magna, at pulvinar erat feugiat id. Ut sit amet congue
+          magna, ut interdum risus. Pellentesque nibh quam est, dictum at nunc
+          bibendum, mattis elementum dui. Curabitur semper porttitor mauris at
+          convallis. Vivamus sagittis metus et metus mollis pulvinar in ac erat.
+          Suspendisse interdum lacus finibus, aliquet purus et, pretium nunc.
+          Aliquam et mi in velit ornare congue. Integer interdum aliquam nibh
+          vitae volutpat. Etiam nec eros eget nibh maximus rutrum a a orci.
+          Vivamus nisi quam, pharetra non congue a, varius ac enim. Nulla nec
+          ligula maximus, vestibulum dui a, mollis sapien. Duis cursus, elit in
+          tempor porta, lorem nunc luctus ligula, quis euismod purus nulla a
+          nisi. Fusce rhoncus elit magna, at pulvinar erat feugiat id. Ut sit
+          amet congue magna, ut interdum risus. Pellentesque nibh quam est,
+          dictum at nunc bibendum, mattis elementum dui. Curabitur semper
+          porttitor mauris at convallis. Vivamus sagittis metus et metus mollis
+          pulvinar in ac erat. Suspendisse interdum lacus finibus, aliquet purus
+          et, pretium nunc. Aliquam et mi in velit ornare congue. Integer
+          interdum aliquam nibh vitae volutpat. Etiam nec eros eget nibh maximus
+          rutrum a a orci. Vivamus nisi quam, pharetra non congue a, varius ac
+          enim. Nulla nec ligula maximus, vestibulum dui a, mollis sapien. Duis
+          cursus, elit in tempor porta, lorem
+        </p>
+        <Navbar />
+      </div>
+    </>
+  );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles.nav}>
+      <p>This a navbar</p>
+    </div>
+  );
+}
