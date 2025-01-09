289bafd3545e540c21fb41b317ee42ff2439e167
alexespin445 <alexreyyy1234@gmail.com>
2025-01-09 02:05:56 -0700

    fixed nav problem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/Navbar.module.css";
 export default function Navbar() {
   return (
     <div className={styles.nav}>
-      <p>This a navbar</p>
+      <p className={styles.text}>This a navbar</p>
     </div>
   );
 }
