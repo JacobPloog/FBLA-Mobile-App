0876b57cfd25f7bd60d1974cb1c8074abdb691ff
Jacob Ploog <jrploog13@gmail.com>
2025-01-09 21:47:36 -0700

    didnt do much

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import styles from "../styles/Navbar.module.css";
+import Icon1 from "./Icon1"
 
 export default function Navbar() {
   return (
     <div className={styles.nav}>
+      <Icon1/>
       <p>This a navbar</p>
     </div>
   );
