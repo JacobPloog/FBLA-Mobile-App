bf7091828ef29b0ecc39ff084f4c6caf79ec1dcd
Jacob Ploog <jploog066@west-mec.org>
2025-01-13 13:54:55 -0700

    poutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// // src/App.jsx
-// import Navbar from "./components/Navbar";
-// // import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styles from "./styles/Home.module.css";
 
 import "./App.css";
@@ -12,14 +10,25 @@ export default function App() {
           <p className={styles.title}>Math</p>
         </div>
         <p className={styles.subjects}>
-          <button className={styles.test}>Test 1</button>
-          <button>Quiz 1</button>
-          <button>Quiz 1</button>
+          <Router>
+            <Routes>
+              <button className={styles.test}>Test 1</button>
+              <button className={styles.quiz}>Quiz 1</button>
+              <button className={styles.quiz}>Quiz 1</button>
+            </Routes>
+          </Router>
         </p>
+        <div className={styles.titleBg}>
+          <p className={styles.title}>English</p>
+        </div>
         <p className={styles.subjects}>
-          <button className={styles.test}>Test 1</button>
-          <button>Quiz 1</button>
-          <button>Quiz 1</button>
+          <Router>
+            <Routes>
+              <button className={styles.test}>Test 1</button>
+              <button className={styles.quiz}>Quiz 1</button>
+              <button className={styles.quiz}>Quiz 1</button>
+            </Routes>
+          </Router>
         </p>
       </div>
     </>
