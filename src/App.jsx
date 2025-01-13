866f1c5150c780239eb31eed46b2c16ef0c10298
Jacob Ploog <jploog066@west-mec.org>
2025-01-13 13:21:57 -0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,16 @@ export default function App() {
   return (
     <>
       <div className={styles.wrapper}>
-        <p className={styles.title}>Math</p>
-        <p className={styles.buttons}>
+        <div className={styles.titleBg}>
+          <p className={styles.title}>Math</p>
+        </div>
+        <p className={styles.subjects}>
           <button className={styles.test}>Test 1</button>
           <button>Quiz 1</button>
           <button>Quiz 1</button>
-          <button>Quiz 1</button>
-          <button>Quiz 1</button>
-          <button>Quiz 1</button>
+        </p>
+        <p className={styles.subjects}>
+          <button className={styles.test}>Test 1</button>
           <button>Quiz 1</button>
           <button>Quiz 1</button>
         </p>
