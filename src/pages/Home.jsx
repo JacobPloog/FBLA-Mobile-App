24c7b72b615629feb1d0ac52656b587d38d2357b
Jacob Ploog <jploog066@west-mec.org>
2025-01-14 14:13:30 -0700

    progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-// import { Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import styles from "../styles/Home.module.css";
 import Logo from "../Logo.jsx";
 import Mascot from "../Mascot.jsx";
+import { useNavigate } from "react-router-dom";
+// import Img1 from "./Img1.png";
 
-// import "./App.css";
 export default function Home() {
+  const navigate = useNavigate();
+  const quizButton = () => {
+    navigate("/quiz");
+  };
   return (
     <>
       <div className={styles.wrapper}>
@@ -22,11 +26,14 @@ export default function Home() {
         <p className={styles.subjects}>
           <button className={styles.test}>Test 1</button>
           <div className={styles.quizWrapper}>
-            <button className={styles.quizButton}>Quiz 1</button>
+            <button
+              onClick={quizButton}
+              className={styles.mathQuizButton}
+            ></button>
             <p>Quiz 1</p>
           </div>
           <div className={styles.quizWrapper}>
-            <button className={styles.quizButton}>Quiz 1</button>
+            <button className={styles.mathQuizButton}></button>
             <p>Quiz 1</p>
           </div>
         </p>
@@ -39,11 +46,11 @@ export default function Home() {
         <p className={styles.subjects}>
           <button className={styles.test}>Test 1</button>
           <div className={styles.quizWrapper}>
-            <button className={styles.quizButton}>Quiz 1</button>
+            <button className={styles.engQuizButton}></button>
             <p>Quiz 1</p>
           </div>
           <div className={styles.quizWrapper}>
-            <button className={styles.quizButton}>Quiz 1</button>
+            <button className={styles.engQuizButton}></button>
             <p>Quiz 1</p>
           </div>
         </p>
@@ -56,11 +63,11 @@ export default function Home() {
         <p className={styles.subjects}>
           <button className={styles.test}>Test 1</button>
           <div className={styles.quizWrapper}>
-            <button className={styles.quizButton}>Quiz 1</button>
+            <button className={styles.hisQuizButton}></button>
             <p>Quiz 1</p>
           </div>
           <div className={styles.quizWrapper}>
-            <button className={styles.quizButton}>Quiz 1</button>
+            <button className={styles.hisQuizButton}></button>
             <p>Quiz 1</p>
           </div>
         </p>
@@ -73,11 +80,11 @@ export default function Home() {
         <p className={styles.subjects}>
           <button className={styles.test}>Test 1</button>
           <div className={styles.quizWrapper}>
-            <button className={styles.quizButton}>Quiz 1</button>
+            <button className={styles.sciQuizButton}></button>
             <p>Quiz 1</p>
           </div>
           <div className={styles.quizWrapper}>
-            <button className={styles.quizButton}>Quiz 1</button>
+            <button className={styles.sciQuizButton}></button>
             <p>Quiz 1</p>
           </div>
         </p>
