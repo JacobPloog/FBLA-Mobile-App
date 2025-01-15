ae07b7895f9313639f0792892c6c2b4b347e83f2
alexespin445 <alexreyyy1234@gmail.com>
2025-01-14 18:05:45 -0700

    quiz will check for correct answer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Styles from "../styles/Quiz.module.css";
 
 export default function Quiz() {
+  const [isCorrect, setIsCorrect] = useState(null);
+
   const questions = [
     {
-      question: "1 + 1",
+      question: "What does 1 + 1 equal;",
       options: ["1", "2", "3", "4"],
       correctAnswer: "2",
     },
   ];
 
+  const checkIfCorrect = (e) => {
+    const selectedOption = e.target.innerHTML;
+    
+
+    console.log("selected:", selectedOption);
+    if (selectedOption === questions[0].correctAnswer) {
+      console.log("Correct");
+      e.target.classList.add("correct")
+      console.log(e.target.classList)
+    } else {
+      console.log("Incorrect");
+    }
+  }
+
   return (
     <>
       <h1>{questions[0].question}</h1>
-      <div>
+      <div className={Styles.options }>
         {questions[0].options.map((option) => {
-          <>
-            <button>{option}</button>
-          </>
+            return <button onClick={checkIfCorrect}>{option}</button>
         })}
       </div>
     </>
